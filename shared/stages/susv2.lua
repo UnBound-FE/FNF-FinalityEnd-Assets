26bfd57ec06ecc9cf2424ab2b7f0e818ee0f77ff
MzkX2 <mokkiol78@gmail.com>
2024-11-10 18:09:20 +0900

    stageess

---
+++ shared/stages/susv2.lua
@@ -0,0 +1,37 @@
+local x = -550
+local y = -140
+
+function onCreate()
+    makeLuaSprite('plate', 'plate', -2150, -1350);
+    setScrollFactor('plate', 1, 1);
+    scaleObject('plate', 0.8, 0.8);
+
+    makeLuaSprite('deadbody', 'deadbody', -1400, -300);
+    setScrollFactor('deadbody', 1, 1);
+    scaleObject('deadbody', 1.50, 1.50);
+
+    makeLuaSprite('screenofdeath', 'screenofdeath', -1050, -300);
+    setScrollFactor('screenofdeath', 1, 1);
+    scaleObject('screenofdeath', 1.5, 1.5);
+
+    makeAnimatedLuaSprite('glitch', 'glitch', -2100, -350);
+    setScrollFactor('glitch', 1, 1);
+    scaleObject('glitch', 2, 2);
+    addAnimationByPrefix('glitch','glitch','glitch',24,false);
+    setBlendMode('dark','multiply')
+
+    
+
+
+    addLuaSprite('screenofdeath', false)
+    addLuaSprite('glitch', false)
+    addLuaSprite('plate', false)
+    addLuaSprite('deadbody', true)
+
+    function onBeatHit()
+        if curBeat %2 == 0 then
+            objectPlayAnimation('glitch', 'glitch')
+
+        end
+    end
+end
