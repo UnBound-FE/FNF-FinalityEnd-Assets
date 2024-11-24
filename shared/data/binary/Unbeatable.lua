40529ea9150e93b70d9483cb8dea180ad1610a25
Vurado-official <vladlavin5@gmail.com>
2024-11-24 12:21:55 +0200

    sabnr

--- shared/data/binary/Unbeatable.lua
+++ shared/data/binary/Unbeatable.lua
@@ -1,19 +1,22 @@
 local vignetteShader = "test5"
 local tvShader = "test3"
+local GlitchShader = "vcr"
 function onCreate()
     if shadersEnabled then
         makeLuaSprite("vigCamShader")
         makeLuaSprite("tvEffect")
+        makeLuaSprite("vcr")
         runHaxeCode([[
             var vignetteShader = "]] .. vignetteShader .. [[";
             var tvEffect = "]] .. tvShader .. [[";
-            
+            var vcr = "]] .. GlitchShader .. [[";
+
             game.initLuaShader(vignetteShader);
             game.initLuaShader(tvEffect);
-            
+
             var shader0 = game.createRuntimeShader(vignetteShader);
             var shader1 = game.createRuntimeShader(tvEffect);
-            
+
             game.getLuaObject("vigCamShader").shader = shader0;
             game.getLuaObject("tvEffect").shader = shader1;
 
